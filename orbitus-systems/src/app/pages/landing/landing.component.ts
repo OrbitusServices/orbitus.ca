4c7437b4dbe63edec8e00ec6a1bd4e639a7bfa9d
FearGannicus <gannicus@exordium.org>
2022-04-07 19:13:22 -0400

    landing page addition

---
+++ orbitus-systems/src/app/pages/landing/landing.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-landing',
+  templateUrl: './landing.component.html',
+  styles: [],
+})
+export class LandingComponent implements OnInit {
+  public isCollapsed = true;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
